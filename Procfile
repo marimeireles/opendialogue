aac0df2e87dc7b3c30408a1208e07954d2bab1fd
marimeireles <marianameireles@protonmail.com>
2023-09-18 19:35:51 -0400

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn commonsdialogue:app
---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+login = LoginManager(app)
+login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+from app import routes, models, errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/commonsdialogue.log', maxBytes=10240,backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
